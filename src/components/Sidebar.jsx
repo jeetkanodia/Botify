594dac06ecbeded35c3d58307f4a0957c2e446d8
jeet kanodia <jeetkanodia@gmail.com>
2023-11-09 04:03:03 +0530

    Add PlayPause and SongCard components, update Sidebar and Discover pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+const Sidebar = () => <div>Sidebar</div>;
 
 export default Sidebar;
+
+// 1:05:54s
+// https://www.youtube.com/watch?v=I1cpb0tYV74
