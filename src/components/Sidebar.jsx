5f56e39ea5e9bac4efc1bc133901d2fc9e6cc779
jeet kanodia <jeetkanodia@gmail.com>
2023-11-10 03:25:23 +0530

    Add mobile menu to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,39 @@
-const Sidebar = () => <div>Sidebar</div>;
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
+
+import logo from "../assets/logo.png";
+import { links } from "../assets/constants";
+
+const NavLinks = ({ handleClick }) => {
+  return (
+    <div className="mt-10">
+      {links.map((link) => (
+        <NavLink
+          key={link.name}
+          to={link.to}
+          className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+          onClick={() => handleClick && handleClick()}
+        >
+          <link.icon className="w-6 h-6 mr-2" />
+          {link.name}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col py-10 w-[240px] px-4 bg-[#191624] ">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+    </>
+  );
+};
 
 export default Sidebar;
-
-// 1:05:54s
-// https://www.youtube.com/watch?v=I1cpb0tYV74
